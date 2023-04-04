b7aa43b92015cfe66e38238458b32861eaf7478b
Ketil Roed <ketilroe@uio.no>
2023-04-04 17:53:08 +0200

    Corrected path to pinning file in ex1; corrected length of slv in ex1 inputs and outputs

--- ex1/quartus/create_quartus_project.tcl
+++ ex1/quartus/create_quartus_project.tcl
@@ -9,7 +9,7 @@ set_global_assignment -name DEVICE 10M50DAF484C7G
 set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files
 
 # Source pinning file
-source ../scripts/de10-lite-pinning.tcl
+source de10-lite-pinning.tcl
 
 # VHDL source files
 set_global_assignment -name VHDL_FILE ../src/ex1.vhd
--- ex1/src/ex1.vhd
+++ ex1/src/ex1.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity ex1 is
         port(
-                sw: in std_logic_vector(7 downto 0);
-                led: out std_logic_vector(7 downto 0)
+                sw: in std_logic_vector(9 downto 0);
+                led: out std_logic_vector(9 downto 0)
         );
 end entity;
 
